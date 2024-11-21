2a5d70edabcee9ca48e3779cad98a125f4b243db
backnback <garam8796@naver.com>
2024-11-21 16:34:32 +0900

    feat: SearchProvider로 검색관련 랜더링만 하는 컴포넌트 생성

--- src/routes/ShareStoryList.js
+++ src/routes/ShareStoryList.js
@@ -2,16 +2,16 @@ import React, { useEffect, useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // useNavigate import 추가
 // import './ShareStoryList.css'; // 스타일 파일 임포트
 import StoryItemList from "../components/StoryItemList";
+import SearchProvider from "../components/SearchProvider";
 import ShareStoryView from './ShareStoryView.js';
 import useModals from '../useModals';
 import { modals } from '../components/Modals';
-import { InputProvider } from '../components/InputProvider';
-import { ButtonProvider } from '../components/ButtonProvider';
 import { StoryTitleProvider } from '../components/TitleProvider.js';
 import { SelectProvider } from '../components/SelectProvider.js';
 import styles from '../assets/styles/css/StoryItemList.module.css';
 import axiosInstance from '../components/AxiosInstance.js';
 import UseScrollAlert from './UseScrollAlert.js';
+
 
 const fetchStoryList = async (accessToken, sortByOption, option, searchQuery, setStoryList, limit, setHasMore) => {
     try {
@@ -182,52 +182,13 @@ const ShareStoryList = () => {
 
     return (
         <div className={styles.list__content__wrap}>
-            <div className='search__wrap'>
-                <form className="search__form" onSubmit={handleSearchSubmit}>
-                    <div className='search__form__wrap'>
-                        <div className={`search__form__item search__form__item__select`}>
-                            <SelectProvider>
-                                <select id="search-select" name="검색어" className={`form__select form__select__search`}
-                                    title='검색' onChange={handleOptionChange}>
-                                    <option value={'0'}>제목</option>
-                                    <option value={'1'}>닉네임</option>
-                                </select>
-                            </SelectProvider>
-                        </div>
-
-                        <div className={`search__form__item search__form__item__input`}>
-                            <InputProvider>
-                                <input
-                                    type='text'
-                                    className={`form__input form__input__search`}
-                                    value={searchQuery}
-                                    onChange={handleSearchChange}
-                                    required
-                                    id='text01'
-                                    name='검색'
-                                    placeholder="검색" />
-                            </InputProvider>
-
-                            <div className={`search__input__item__button`}>
-                                {searchQuery && <ButtonProvider width={'icon'} className={`button__item__x`}>
-                                    <button type="button" className={`button button__icon button__icon__x`} onClick={handleSearchDelete}>
-                                        <span className={`blind`}>삭제</span>
-                                        <i className={`icon icon__x__black`}></i>
-                                    </button>
-                                </ButtonProvider>}
-
-                                <ButtonProvider width={'icon'} className={`button__item__search`}>
-                                    <button type="button" className={`button button__icon button__icon__search`} onClick={handleSearchSubmit}>
-                                        <span className={`blind`}>검색</span>
-                                        <i className={`icon__search`}></i>
-                                    </button>
-                                </ButtonProvider>
-                            </div>
-
-                        </div>
-                    </div>
-                </form>
-            </div>
+            <SearchProvider
+                handleSearchSubmit={handleSearchSubmit}
+                handleOptionChange={handleOptionChange}
+                searchQuery={searchQuery}
+                handleSearchChange={handleSearchChange}
+                handleSearchDelete={handleSearchDelete}
+            />
 
             <StoryTitleProvider
                 title={'공유 스토리'}
--- src/routes/StoryList.js
+++ src/routes/StoryList.js
@@ -5,6 +5,7 @@ import { StoryAddContext } from '../components/StoryItem';
 import StoryItemList from '../components/StoryItemList';
 import StoryAddForm from './StoryAddForm';
 import StoryEditModal from '../components/StoryEditModal';
+import SearchProvider from "../components/SearchProvider";
 import StoryView from './StoryView.js';
 import useModals from '../useModals';
 import { modals } from '../components/Modals';
@@ -199,7 +200,7 @@ const MyStoryList = () => {
         };
 
         loadAccessTokenAndFetchStories();
-    }, [sortBy, searchQuery, limit, navigate]);
+    }, [sortBy, limit, navigate]);
 
     useEffect(() => {
         if (accessToken) {
@@ -210,50 +211,12 @@ const MyStoryList = () => {
 
     return (
         <div className={styles.list__content__wrap}>
-            <div className='search__wrap'>
-                <form className="search__form" onSubmit={handleSearchSubmit}>
-                    <div className='search__form__wrap'>
-                        <div className={`search__form__item search__form__item__select`}>
-                            <SelectProvider>
-                                <select id="search-select" name="검색어" className={`form__select form__select__search`} title='검색'>
-                                    <option value={'0'}>제목</option>
-                                </select>
-                            </SelectProvider>
-                        </div>
-
-                        <div className={`search__form__item search__form__item__input`}>
-                            <InputProvider>
-                                <input
-                                    type='text'
-                                    className={`form__input form__input__search`}
-                                    value={searchQuery}
-                                    onChange={handleSearchChange}
-                                    required
-                                    id='text01'
-                                    name='검색'
-                                    placeholder="검색" />
-                            </InputProvider>
-
-                            <div className={`search__input__item__button`}>
-                                {searchQuery && <ButtonProvider width={'icon'} className={`button__item__x`}>
-                                    <button type="button" className={`button button__icon button__icon__x`} onClick={handleSearchDelete}>
-                                        <span className={`blind`}>삭제</span>
-                                        <i className={`icon icon__x__black`}></i>
-                                    </button>
-                                </ButtonProvider>}
-
-                                <ButtonProvider width={'icon'} className={`button__item__search`}>
-                                    <button type="button" className={`button button__icon button__icon__search`} onClick={handleSearchSubmit}>
-                                        <span className={`blind`}>검색</span>
-                                        <i className={`icon__search`}></i>
-                                    </button>
-                                </ButtonProvider>
-                            </div>
-
-                        </div>
-                    </div>
-                </form>
-            </div>
+            <SearchProvider
+                handleSearchSubmit={handleSearchSubmit}
+                searchQuery={searchQuery}
+                handleSearchChange={handleSearchChange}
+                handleSearchDelete={handleSearchDelete}
+            />
 
             <StoryTitleProvider
                 title={'내 스토리'}
