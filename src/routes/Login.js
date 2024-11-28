07141758c8d9ee91395d3865e80529ebc2e6db4d
uripup <airin00100@gmail.com>
2024-11-28 14:53:04 +0900

    fix: 클라우드 서버 호환

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -62,7 +62,7 @@ function Login() {
 
     const handleGoogleLogin = () => {
         // Google OAuth 로그인 엔드포인트로 리디렉트
-        window.location.href = 'http://localhost:8080/oauth2/authorization/google';
+        window.location.href = 'http://go.remapber.p-e.kr/login/oauth2/code/google';
     };
 
 
